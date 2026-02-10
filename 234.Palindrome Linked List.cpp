a085c8f6d238bec9235e09fad40d414b2d78dc1e
Alisha-bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-02-10 11:32:24 +0530

    Add palindrome linked list solution
    Implement isPalindrome function to check if a linked list is a palindrome.

---
+++ 234.Palindrome Linked List.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    // https://leetcode.com/problems/palindrome-linked-list/
+bool isPalindrome(ListNode* head) {
+
+
+        ListNode* curr=head;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        ListNode* prev=NULL;
+        ListNode*midprev=NULL;
+        while(fast!=NULL and fast->next!=NULL){
+            fast=fast->next->next;
+            midprev=slow;
+            slow=slow->next;
+
+        }
+        ListNode* mid=slow;
+        curr=mid;
+        while(curr){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        if(!midprev)return true;
+        midprev->next=prev;
+        ListNode* curr1=prev;
+        curr=head;
+        while(curr!=prev){
+            if(curr->val!=curr1->val)return false;
+            curr=curr->next;
+            curr1=curr1->next;
+        }
+        return true;
+    }
+};
