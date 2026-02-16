a0144d376091a4bb479978a2a96defebd4ff71c7
Alisha-bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-02-16 15:21:33 +0530

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        
+        for(auto i:nums){
+            mp[i]++;
+        }
+        int ans=0;
+        for(auto i:mp){
+            if(i.second==1){
+                ans=i.first;
+            }
+
+        }
+        return ans;
+    }
+};
