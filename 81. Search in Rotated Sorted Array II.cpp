40d465b060e96e67b3b0edcaf55c5b78ddd457e8
Alisha-bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-02-17 13:20:21 +0530

    Create 81. Search in Rotated Sorted Array II.cpp

---
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        //.....BRUTE FORCE....
+        
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        //......BINARY SEARCH.....
+
+        int l=0;
+        int h=n-1;
+        
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[l] == nums[mid] && nums[mid] == nums[h]) {
+                l++;
+                h--;
+            }
+            else if(nums[mid]>=nums[l]){
+                if(target>=nums[l] && target<nums[mid]){
+                    h=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(target>nums[mid] && target<=nums[h]){
+                    l=mid+1;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
