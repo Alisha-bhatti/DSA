907671eb51fffb5434e5b8c45c66d43e3dd873d5
Alisha-bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-02-23 22:57:24 +0530

    Add maximumGap function to find max gap in array
    Implement a function to find the maximum gap between sorted elements.

---
+++ 164. Maximum Gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+     int n=nums.size()   ;
+     if(n<2) return 0;
+     sort(nums.begin(),nums.end());
+     int gap=0;
+     for(int i=0;i<n-1;i++){
+        int a=nums[i+1]-nums[i];
+        if(a>gap){
+            gap=a;
+        }
+     }
+     return gap;
+    }
+};
