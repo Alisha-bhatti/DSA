12fe171f8f836925293ef64dea12bddf893c4e94
Alisha-bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-02-26 15:47:44 +0530

    Create 2326. Spiral Matrix IV.cpp

---
+++ 2326. Spiral Matrix IV.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> arr(m, vector<int>(n,-1));
+
+        int minr = 0 ;
+        int maxr = m-1;
+        int minc=0;
+        int maxc = n-1;
+        int count = 0;
+       ListNode* temp=head;
+        while(minr<=maxr && minc<=maxc){
+        //right
+        for(int j=minc;j<=maxc;j++){
+            if(temp==NULL) return arr;
+            arr[minr][j]=temp->val;
+            temp=temp->next;
+        }
+        minr++;
+        //down
+        for(int i=minr;i<=maxr;i++){
+             if(temp==NULL) return arr;
+            arr[i][maxc]=temp->val;
+            temp=temp->next;
+        }
+        maxc--;
+        
+        //left
+        for(int j=maxc;j>=minc;j--){
+             if(temp==NULL) return arr;
+           arr[maxr][j]=temp->val;
+           temp=temp->next;
+        }
+        maxr--;
+        
+        //up
+        for(int i=maxr;i>=minr;i--){
+             if(temp==NULL) return arr;
+            arr[i][minc]=temp->val;
+           temp=temp->next;
+        }
+        minc++;
+        if(minr>maxr || minc>maxc) break;
+    }
+   
+     return arr;
+    
+    
+    }
+};
