4923b0b9965f1315810ff1be1ae0f70bc683ba19
Alisha Bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-07-09 11:21:20 +0530

    Add comments for clarity on K-th symbol algorithm
    Added comments to explain the K-th symbol in grammar logic.

--- 779. K-th Symbol in Grammar.cpp
+++ 779. K-th Symbol in Grammar.cpp
@@ -8,3 +8,14 @@ public:
         return kthGrammar(n-1, (k+1)/2);  // if index odd , flip the parent bit
     }
 };
+
+// 0->01
+// 1->10
+
+// row1 -> 0
+// row2 -> 01
+// row3 -> 0110
+// row4 -> 01101001
+
+// k is index, and it is 1 based indexing
+// k/2 as alues keep decreasing by half, row4 ->8 values then row3 -> 4 values and so on.
