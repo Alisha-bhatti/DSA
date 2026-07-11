1fcf86c703e2c3c56ce7ca41ae6eae402437c604
Alisha Bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-07-11 13:51:27 +0530

    Implement array to linked list conversion

---
+++ Array to Linked List.cpp
@@ -0,0 +1,31 @@
+/*
+// Representation of a node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* arrayToList(vector<int>& arr) {
+        // code here
+        if (arr.empty()) return nullptr;
+
+    Node* head = new Node(arr[0]);
+    Node* temp = head;
+
+    for (int i = 1; i < arr.size(); i++) {
+        temp->next = new Node(arr[i]);
+        temp = temp->next;
+    }
+
+    return head;
+    }
+};
