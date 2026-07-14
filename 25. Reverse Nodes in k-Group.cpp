ccdabf12b33d24b5a0a72a73269ae8d6700db0c9
Alisha Bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-07-14 11:44:40 +0530

    Add reverseKGroup function for linked list reversal
    Implement reverseKGroup function to reverse nodes in k-group for a singly-linked list.

---
+++ 25. Reverse Nodes in k-Group.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode *curr=head;
+        int cnt=0;
+        while(curr && cnt<k){
+            curr=curr->next;
+            cnt++;
+        }
+        if(cnt<k) return head;
+        curr=head;
+        ListNode *next=NULL,*prev=NULL;
+        cnt=0;
+        while(cnt<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            cnt++;
+        }
+        head->next=reverseKGroup(curr,k);
+        return prev;
+    }
+};
