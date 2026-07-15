1e7b68493f973cc9bb03e7b209ed67bcd9fd1c72
Alisha Bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-07-15 11:46:16 +0530

    Add solution for Baseball Game problem
    Implement a solution for the Baseball Game problem using a stack to calculate the total score based on operations.

---
+++ 682. Baseball Game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        stack<int>st;
+        for(string s :operations){
+            if(s=="C") st.pop();
+            else if(s=="D"){
+                int top=st.top();
+                st.push(2*top);
+            }
+            else if(s=="+"){
+                int top=st.top();
+                st.pop();
+                int secondTop=st.top();
+                st.push(top);
+                st.push(top+secondTop);
+            }
+             else st.push(stoi(s));
+
+            }
+            int sum=0;
+            while(!st.empty()){
+                sum+=st.top();
+                st.pop();
+            }
+            return sum;
+        
+    }
+};
