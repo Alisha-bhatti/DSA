50170f2ea002af091d9ea213910f2c20ac2c4148
Alisha Bhatti <147086958+Alisha-bhatti@users.noreply.github.com>
2026-07-21 19:23:44 +0530

    Create 1381. Design a Stack With Increment Operation.cpp

---
+++ 1381. Design a Stack With Increment Operation.cpp
@@ -0,0 +1,42 @@
+class CustomStack {
+public:
+    vector<int> st;
+    vector<int> inc;
+    int maxSize;
+
+    CustomStack(int maxSize) {
+        this->maxSize = maxSize;
+    }
+
+    void push(int x) {
+        if (st.size() == maxSize)
+            return;
+
+        st.push_back(x);
+        inc.push_back(0);
+    }
+
+    int pop() {
+        if (st.empty())
+            return -1;
+
+        int i = st.size() - 1;
+        int ans = st[i] + inc[i];
+
+        if (i > 0)
+            inc[i - 1] += inc[i];
+
+        st.pop_back();
+        inc.pop_back();
+
+        return ans;
+    }
+
+    void increment(int k, int val) {
+        if (st.empty())
+            return;
+
+        int i = min(k, (int)st.size()) - 1;
+        inc[i] += val;
+    }
+};
